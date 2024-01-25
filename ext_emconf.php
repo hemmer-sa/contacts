1eb9007be3d4c754e78f6bb428e841601d304767
Xavier Perseguers <xavier@causal.ch>
2024-01-25 09:48:11 +0100

    [TASK] Bring back compabibility with TYPO3 v11
    There is no reasons to migrate this extension and only make it
    compatible with TYPO3 v12 since it was previously compatible
    with both TYPO3 v10 and v11.
    
    As TYPO3 v10 may now be considered obsolete and since there is
    an official release supporting both TYPO3 v10 and v11, we choose
    to further keep compatibility with TYPO3 v11 and v12 only.

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+$EM_CONF['contacts'] = [
+    'title' => 'Contacts',
+    'description' => 'Contacts can handle persons and companies and relations to each other. With multiple addresses and phone numbers Contacts can be a modern replacement for tt_address.',
+    'category' => 'plugin',
+    'author' => 'Daniel Gohlke',
+    'author_email' => 'ext.contacts@extco.de',
+    'author_company' => 'extco.de UG (haftungsbeschränkt)',
+    'shy' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'stable',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '4.1.0',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.4.0-8.3.99',
+            'typo3' => '11.5.0-12.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
